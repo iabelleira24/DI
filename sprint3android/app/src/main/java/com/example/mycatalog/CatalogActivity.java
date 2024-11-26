af55fde191c387471cb5ea9fd69bd6aeb0e47b85
iabelleirav24 <iabelleirav@fpcoruna.afundacion.org>
2024-11-26 14:03:05 +0100

    Arreglado el error que tenia en la creación del proyecto (Ejercicio1)

--- sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,8 +1,6 @@
 package com.example.mycatalog;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,13 +14,7 @@ public class CatalogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_catalog);
-        Button navigateButton = findViewById(R.id.btnNavigateDetail);
-        navigateButton.setOnClickListener(v -> {
-            Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
-            startActivity(intent);
-        });
-
+        setContentView(R.layout.catalog_activity);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
